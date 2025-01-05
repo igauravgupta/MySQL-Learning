a72fda4aeae31f3395c2556c0c7c3f973133643c
Gaurav Gupta <itisgauravgupta@gmail.com>
2025-01-06 00:06:41 +0530

    BASICS-COMPLETE

--- 0.Beginner - Parks_and_Rec_Create_db.sql
+++ 0.Beginner - Parks_and_Rec_Create_db.sql
@@ -8,30 +8,32 @@ CREATE DATABASE IF NOT EXISTS `Parks_and_Recreation`;
 
 USE `Parks_and_Recreation`;
 
+-- Create the employee_demographics table
 CREATE TABLE employee_demographics (
-  employee_id INT NOT NULL,
+  employee_id SERIAL PRIMARY KEY,
   first_name VARCHAR(50),
   last_name VARCHAR(50),
   age INT,
   gender VARCHAR(10),
-  birth_date DATE,
-  PRIMARY KEY (employee_id)
+  birth_date DATE
 );
 
+-- Create the employee_salary table
 CREATE TABLE employee_salary (
   employee_id INT NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   occupation VARCHAR(50),
   salary INT,
-  dept_id INT
+  dept_id INT,
+  FOREIGN KEY (employee_id) REFERENCES employee_demographics(employee_id)
 );
 
-
+-- Insert data into employee_demographics
 INSERT INTO employee_demographics (employee_id, first_name, last_name, age, gender, birth_date)
 VALUES
-(1,'Leslie', 'Knope', 44, 'Female','1979-09-25'),
-(3,'Tom', 'Haverford', 36, 'Male', '1987-03-04'),
+(1, 'Leslie', 'Knope', 44, 'Female', '1979-09-25'),
+(3, 'Tom', 'Haverford', 36, 'Male', '1987-03-04'),
 (4, 'April', 'Ludgate', 29, 'Female', '1994-03-27'),
 (5, 'Jerry', 'Gergich', 61, 'Male', '1962-08-28'),
 (6, 'Donna', 'Meagle', 46, 'Female', '1977-07-30'),
@@ -42,30 +44,29 @@ VALUES
 (11, 'Mark', 'Brendanawicz', 40, 'Male', '1983-06-14'),
 (12, 'Craig', 'Middlebrooks', 37, 'Male', '1986-07-27');
 
-
+-- Insert data into employee_salary
 INSERT INTO employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
 VALUES
-(1, 'Leslie', 'Knope', 'Deputy Director of Parks and Recreation', 75000,1),
-(2, 'Ron', 'Swanson', 'Director of Parks and Recreation', 70000,1),
-(3, 'Tom', 'Haverford', 'Entrepreneur', 50000,1),
-(4, 'April', 'Ludgate', 'Assistant to the Director of Parks and Recreation', 25000,1),
-(5, 'Jerry', 'Gergich', 'Office Manager', 50000,1),
-(6, 'Donna', 'Meagle', 'Office Manager', 60000,1),
-(7, 'Ann', 'Perkins', 'Nurse', 55000,4),
-(8, 'Chris', 'Traeger', 'City Manager', 90000,3),
-(9, 'Ben', 'Wyatt', 'State Auditor', 70000,6),
+(1, 'Leslie', 'Knope', 'Deputy Director of Parks and Recreation', 75000, 1),
+(2, 'Ron', 'Swanson', 'Director of Parks and Recreation', 70000, 1),
+(3, 'Tom', 'Haverford', 'Entrepreneur', 50000, 1),
+(4, 'April', 'Ludgate', 'Assistant to the Director of Parks and Recreation', 25000, 1),
+(5, 'Jerry', 'Gergich', 'Office Manager', 50000, 1),
+(6, 'Donna', 'Meagle', 'Office Manager', 60000, 1),
+(7, 'Ann', 'Perkins', 'Nurse', 55000, 4),
+(8, 'Chris', 'Traeger', 'City Manager', 90000, 3),
+(9, 'Ben', 'Wyatt', 'State Auditor', 70000, 6),
 (10, 'Andy', 'Dwyer', 'Shoe Shiner and Musician', 20000, NULL),
 (11, 'Mark', 'Brendanawicz', 'City Planner', 57000, 3),
-(12, 'Craig', 'Middlebrooks', 'Parks Director', 65000,1);
+(12, 'Craig', 'Middlebrooks', 'Parks Director', 65000, 1);
 
-
-
+-- Create the parks_departments table
 CREATE TABLE parks_departments (
-  department_id INT NOT NULL AUTO_INCREMENT,
-  department_name varchar(50) NOT NULL,
-  PRIMARY KEY (department_id)
+  department_id SERIAL PRIMARY KEY,
+  department_name VARCHAR(50) NOT NULL
 );
 
+-- Insert data into parks_departments
 INSERT INTO parks_departments (department_name)
 VALUES
 ('Parks and Recreation'),
@@ -74,16 +75,3 @@ VALUES
 ('Healthcare'),
 ('Library'),
 ('Finance');
-
-
-
-
-
-
-
-
-
-
-
-
-
